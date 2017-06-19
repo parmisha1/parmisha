27f8369c8fa440136932e760f27f03fa31ff88f0
parmisha <parmisha@192.168.0.21>
2017-06-19 14:07:28 +0530

    opration2 change variable

--- src/com/pack/Employee.java
+++ src/com/pack/Employee.java
@@ -4,10 +4,10 @@ public class Employee {
 	
 	private int empId;
 	private String empName;
-	private int sal;
+	
 	@Override
 	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + ", sal=" + sal + "]";
+		return "Employee [empId=" + empId + ", empName=" + empName  + "]";
 	}
 	public int getEmpId() {
 		return empId;
@@ -21,11 +21,5 @@ public class Employee {
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
-	public int getSal() {
-		return sal;
-	}
-	public void setSal(int sal) {
-		this.sal = sal;
-	}
-
+	
 }
